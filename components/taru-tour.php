d2644680d765286753db528a2c253da5a5ae480a
Handy Dannu Wijaya <handydannu@users.noreply.github.com>
2021-02-19 14:31:46 +0700

    Update taru-tour.php

--- components/taru-tour.php
+++ components/taru-tour.php
@@ -7,9 +7,38 @@
 			  <iframe class="embed-responsive-item" src="https://kuula.co/share/collection/7Pybf?fs=1&vr=1&zoom=-1&thumbs=0&margin=9&card=0&info=0&logo=-1" allowfullscreen></iframe>
 			</div>	
 
-			<div class="embed-responsive embed-responsive-16by9 mt-2">
-			  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/xSjMdB4cies?rel=0" allowfullscreen></iframe>
-			</div>	
+			   <div id="taru-video" class="carousel slide" data-ride="carousel" data-interval="false"><!-- 
+			      <ol class="carousel-indicators">
+			        <li data-target="#taru-video" data-slide-to="0" class="active"></li>
+			        <li data-target="#taru-video" data-slide-to="1"></li>
+			        <li data-target="#taru-video" data-slide-to="2"></li>
+			      </ol> -->
+
+			      <div class="carousel-inner">
+			        <div class="carousel-item active">      
+						<div class="embed-responsive embed-responsive-16by9 mt-2">
+						  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/xSjMdB4cies?rel=0" allowfullscreen></iframe>
+						</div>
+			        </div>
+			        <div class="carousel-item">
+			            <div class="carousel-item active">      
+							<div class="embed-responsive embed-responsive-16by9 mt-2">
+							  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/-G10EpNfGwg?rel=0" allowfullscreen></iframe>
+							</div>
+			       		 </div>
+			        </div><!-- 
+			        <div class="carousel-item">
+			          <img class="d-block w-100 img-fluid" src="../assets/img/tarubhawana/park-3.jpg">
+			        </div> -->
+			      </div>
+
+			      <a class="carousel-control-prev" href="#taru-video" role="button" data-slide="prev">
+			        <i class="fas fa-caret-left fa-5x" style="color: #038A7C"></i>
+			      </a>
+			      <a class="carousel-control-next" href="#taru-video" role="button" data-slide="next">
+			       <i class="fas fa-caret-right fa-5x" style="color: #038A7C"></i>
+			      </a>
+			    </div>	
 		</div>
 	<div class="col-md-2 col-sm-2 col-xs-2"></div>
 </div>
